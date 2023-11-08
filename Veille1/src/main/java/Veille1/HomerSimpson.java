748a4673b65a2875746d85f5c21813989bcc90db
EloElodie18 <elodierichard@yahoo.fr>
2023-11-08 21:49:51 +0100

    CommitTest3

--- Veille1/src/main/java/Veille1/HomerSimpson.java
+++ Veille1/src/main/java/Veille1/HomerSimpson.java
@@ -1,7 +1,7 @@
 package Veille1;
 
 public class HomerSimpson { /*creation de la fonction qui est pblc dc ouverte nommée HomerSimpson*/
-	public static String drinkBeer() { /*création d'une methode de type string nommée... qui retourne...*/
+	public static String drinkBeer() { /*création d'une methode de type string nommée... qui retourne.*/
 		return "I love Duff";
 	}
 	public static String eat () {
